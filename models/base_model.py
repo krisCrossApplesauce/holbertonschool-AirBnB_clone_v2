1f3fbe256edee413683e3d0b01e56966c3bf8636
root <Caramonh@yahoo.com>
2023-07-04 09:39:16 -0700

    pycode

--- models/base_model.py
+++ models/base_model.py
@@ -8,12 +8,12 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), primary_key=True, nullable=False)
     created_at = Column(Datetime, nullable=False, default=datetime.utcnow())
     updated_at = Column(Datetime, nullable=False, default=datetime.utcnow())
-
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
