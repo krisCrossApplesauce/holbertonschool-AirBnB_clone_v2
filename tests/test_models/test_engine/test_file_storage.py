78fb81a73586dd2a8313b7f78e36a27b61258aba
root <Caramonh@yahoo.com>
2023-07-05 09:08:18 -0700

    test_filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,6 +6,7 @@ from models import storage
 import os
 
 
+@unittest.skipIf(os.getenv("HBNB_ENV") is not None, "Testing DBStorage")
 class test_fileStorage(unittest.TestCase):
     """ Class to test the file storage method """
 
@@ -31,9 +32,8 @@ class test_fileStorage(unittest.TestCase):
     def test_new(self):
         """ New object is correctly added to __objects """
         new = BaseModel()
-        for obj in storage.all().values():
-            temp = obj
-        self.assertTrue(temp is obj)
+        new.save()
+        self.assertIn(new, storage.all().values())
 
     def test_all(self):
         """ __objects is properly returned """
@@ -63,11 +63,12 @@ class test_fileStorage(unittest.TestCase):
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
         new = BaseModel()
-        storage.save()
+        new.save()
+        bm_id = new.to_dict()['id']
         storage.reload()
-        for obj in storage.all().values():
-            loaded = obj
-        self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
+        expected_key = 'BaseModel.' + bm_id
+        keys = list(storage.all().keys())
+        self.assertIn(expected_key, keys)
 
     def test_reload_empty(self):
         """ Load from an empty file """
@@ -97,10 +98,11 @@ class test_fileStorage(unittest.TestCase):
     def test_key_format(self):
         """ Key is properly formatted """
         new = BaseModel()
-        _id = new.to_dict()['id']
-        for key in storage.all().keys():
-            temp = key
-        self.assertEqual(temp, 'BaseModel' + '.' + _id)
+        bm_id = new.to_dict()['id']
+        new.save()
+        expected_key = 'BaseModel.' + bm_id
+        keys = list(storage.all().keys())
+        self.assertIn(expected_key, keys)
 
     def test_storage_var_created(self):
         """ FileStorage object storage created """
