8c9a2a52bc8869d145c3c4452c6a0219583c5e42
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-14 13:05:07 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" /python/<text> """
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python():
+    return "Python {}".format(text.replace("_", " "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
