3787117f394f5b28ee3d013548748f4281c96f3e
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-14 14:19:52 -0700

    4: /number/<n> -> /number/<int:n> and got rid of if statement

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,10 +5,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    if n is int:
-        return "{} is a number".format(n)
+    return "{} is a number".format(n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
