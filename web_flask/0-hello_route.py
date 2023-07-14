2996b03ed13cb27c67e9edb14422647e7eb30185
root <Caramonh@yahoo.com>
2023-07-14 10:45:42 -0700

    0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-""" Starts a Flask web application """
+"""Starts a Flask web application"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.rpute('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
