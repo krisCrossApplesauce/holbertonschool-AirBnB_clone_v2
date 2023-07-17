06513f8336e273abad787283a2cd32e4bbeb76f4
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-17 13:23:57 -0700

    7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -13,7 +13,8 @@ def teardown(exception):
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
-    return render_template('7-states_list.html', states=storage.all("State"))
+    states = storage.all("State")
+    return render_template('7-states_list.html', states=storage.values())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
