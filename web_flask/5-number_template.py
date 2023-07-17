d44610660912fa27e63ef87e02c03bfe51a255e4
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-17 12:11:43 -0700

    5: in render_template: 'templates/5-number.html' -> '5-number.html'

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    return render_template('templates/5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
