6f12e8b0ab7cfafe86b491948caf9a41677b8c1b
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-17 13:28:58 -0700

    7: typo

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -14,7 +14,7 @@ def teardown(exception):
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all("State")
-    return render_template('7-states_list.html', states=storage.values())
+    return render_template('7-states_list.html', states=states.values())
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
