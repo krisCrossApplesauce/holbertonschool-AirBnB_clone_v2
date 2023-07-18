4b2347100cef7151de1c7c47b8098706ac8097ee
krisCrossApplesauce <karisrr2203@gmail.com>
2023-07-18 12:10:14 -0700

    8

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" /cities_by_states """
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontent()
+def teardown(exception):
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all("States")
+    cities = storage.all("Cities")
+    return render_template('8-cities_by_states.html', states=states.values(), cities=cities.values())
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
